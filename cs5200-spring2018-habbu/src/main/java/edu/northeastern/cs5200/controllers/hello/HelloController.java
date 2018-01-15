b1e9d0d5d1b5d02e536086b6714851b3e1990688
srinidhishankar <srinidhi.shankar.habbu@gmail.com>
2018-01-15 16:39:46 -0500

    First DB application

---
+++ cs5200-spring2018-habbu/src/main/java/edu/northeastern/cs5200/controllers/hello/HelloController.java
@@ -0,0 +1,19 @@
+package edu.northeastern.cs5200.controllers.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	@RequestMapping("/api/hello/string")
+	public String sayHello() {
+		return "Hello Srinidhi Shankar Habbu!";
+	}
+	
+	@RequestMapping("/api/hello/object")
+	public HelloObject sayHelloObject() {
+		HelloObject obj = new HelloObject("Hello Srinidhi Shanakr Habbu!");
+		return obj;
+	}
+
+}
